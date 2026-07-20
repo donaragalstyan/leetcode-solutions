782d51538ac934f2512e0bbf7037ad7fb6c50e91
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-07-19 18:55:36 -0700

    Time: 25 ms (27.95%) | Memory: 46.8 MB (21.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -10,33 +10,37 @@ class Solution {
             charToCount.put(ch, charToCount.getOrDefault(ch, 0) + 1);
         }
 
-        char[] curr = new char[s1.length()];
-        for (int i = 0; i < s1.length(); i++) {
-            curr[i] = s2.charAt(i);
-        }
-
         int start = 0;
 
-        for (int right = s1.length(); right <= s2.length(); right++) {
+        for (int right = 0; right < s2.length(); right++) {
+            char ch = s2.charAt(right);
 
-            HashMap<Character, Integer> windowCount = new HashMap<>();
-            for (char ch : curr) {
-                windowCount.put(ch, windowCount.getOrDefault(ch, 0) + 1);
+            if (charToCount.containsKey(ch)) {
+                charToCount.put(ch, charToCount.get(ch) - 1);
             }
-            if (windowCount.equals(charToCount)) {
+
+            if (right - start + 1 > s1.length()) {
+                char leftChar = s2.charAt(start);
+                if (charToCount.containsKey(leftChar)) {
+                    charToCount.put(leftChar, charToCount.get(leftChar) + 1);
+                }
+                start++;
+            }
+
+            if (right - start + 1 == s1.length() && allZero(charToCount)) {
                 return true;
             }
-
-            if (right == s2.length()) {
-                break;
-            }
-
-            for (int i = 1; i < curr.length; i++) {
-                curr[i - 1] = curr[i];
-            }
-            curr[curr.length - 1] = s2.charAt(right);
         }
 
         return false;
+    }
+
+    private boolean allZero(HashMap<Character, Integer> map) {
+        for (int count : map.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
