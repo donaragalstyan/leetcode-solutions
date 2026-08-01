a9ac86520448580a06db37734fad39be15aba62c
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-07-31 20:02:36 -0700

    Time: 1 ms (51.87%) | Memory: 42.7 MB (57.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int length = 2; length <= n; length++) {
+            for (int left = 0; left + length - 1 < n; left++) {
+                int right = left + length - 1;
+
+                int takeLeft = nums[left] - dp[left + 1][right];
+                int takeRight = nums[right] - dp[left][right - 1];
+
+                dp[left][right] = Math.max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}
