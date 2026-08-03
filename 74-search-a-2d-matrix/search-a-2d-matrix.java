794b3f06bca0bdb9b4913be57663f789f1ea7dd6
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-08-02 18:38:29 -0700

    Time: 0 ms (100.00%) | Memory: 44 MB (24.86%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            int row = middle / cols;
+            int col = middle % cols;
+
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return false;
+    }
+}
