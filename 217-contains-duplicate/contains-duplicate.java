5464ba1ac17e190de173863054f05d74cd273c6b
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-08-17 10:15:49 -0700

    Time: 18 ms (71.45%) | Memory: 107.8 MB (57.36%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            } else {
+                set.add(i);
+            }
+        }
+
+
+        return false;
+    }
+}
