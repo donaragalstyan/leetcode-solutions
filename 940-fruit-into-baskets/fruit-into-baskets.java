3a3cb4c5db77279f9fbd483c21fae64932369ed4
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-09-03 11:41:04 -0700

    Time: 52 ms (83.45%) | Memory: 66.6 MB (93.43%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,33 +1,31 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int left = 0;
-        int maxTotal = 0;
-
         HashMap<Integer, Integer> fruitToCount = new HashMap<>();
+        int left = 0;
+        int right;
+        int maxNum = Integer.MIN_VALUE;
+        int curr = 0;
 
-        for (int right = 0; right < fruits.length; ++right) {
-
+        for (right = 0; right < fruits.length; ++right) {
             fruitToCount.put(fruits[right], fruitToCount.getOrDefault(fruits[right], 0) + 1);
+            curr++;
 
-            while (fruitToCount.size() > 2) {
-
-                int fruitLeaving = fruits[left];
-
-                fruitToCount.put(
-                    fruitLeaving,
-                    fruitToCount.get(fruitLeaving) - 1
-                );
-
-                if (fruitToCount.get(fruitLeaving) == 0) {
-                    fruitToCount.remove(fruitLeaving);
+            if (fruitToCount.size() > 2) {
+                if (fruitToCount.get(fruits[left]) > 1) {
+                    fruitToCount.put(fruits[left], fruitToCount.get(fruits[left])-1);
+                } else {
+                    fruitToCount.remove(fruits[left]);
                 }
 
                 left++;
+                curr--;
             }
 
-            maxTotal = Math.max(maxTotal, right - left + 1);
+            maxNum = Math.max(maxNum, curr);
         }
 
-        return maxTotal;
+
+        return maxNum;
+        
     }
 }
