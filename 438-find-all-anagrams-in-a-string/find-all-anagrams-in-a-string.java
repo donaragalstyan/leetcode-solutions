ded629aa148af185ba6eb75d1f9d9f883777727e
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-09-07 16:41:32 -0700

    Time: 55 ms (24.47%) | Memory: 48 MB (10.12%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,48 +1,47 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-
+        List<Integer> res = new ArrayList<>();
         if (p.length() > s.length()) {
-            return result;
+            return res;
         }
-
-        HashMap<Character, Integer> anagram = new HashMap<>();
-        for (Character ch : p.toCharArray()) {
-            anagram.put(ch, anagram.getOrDefault(ch, 0) + 1);
+        HashMap<Character, Integer> charToCount = new HashMap<>();
+        for(Character ch : p.toCharArray()) {
+            charToCount.put(ch, charToCount.getOrDefault(ch, 0) + 1);
         }
-
-        HashMap<Character, Integer> current = new HashMap<>();
-
-        int lengthOfP = p.length();
-        char[] sToArray = s.toCharArray();
 
         int p1 = 0;
-        int p2 = 0;
+        int p2 = p.length();
 
-        while (p2 < sToArray.length) {
+        HashMap<Character, Integer> curr = new HashMap<>();
+        for (int i = p1; i < p2; ++i) {
+            char ch = s.charAt(i);
+            curr.put(ch, curr.getOrDefault(ch, 0) + 1);
+        }
 
-            current.put(sToArray[p2], current.getOrDefault(sToArray[p2], 0) + 1);
-
-            if (p2 - p1 + 1 > lengthOfP) {
-                if (current.get(sToArray[p1]) == 1) {
-                    current.remove(sToArray[p1]);
-                } else {
-                    current.put(
-                        sToArray[p1],
-                        current.get(sToArray[p1]) - 1
-                    );
-                }
-
-                p1++;
+        while (true) {
+            if (curr.equals(charToCount)) {
+                res.add(p1);
             }
 
-            if (p2 - p1 + 1 == lengthOfP && current.equals(anagram)) {
-                result.add(p1);
+            if (p2 == s.length()) {
+                break;
             }
 
+            char leaving = s.charAt(p1);
+
+            if (curr.get(leaving) == 1) {
+                curr.remove(leaving);
+            } else {
+                curr.put(leaving, curr.get(leaving) - 1);
+            }
+
+            char entering = s.charAt(p2);
+            curr.put(entering, curr.getOrDefault(entering, 0) + 1);
+
+            p1++;
             p2++;
         }
 
-        return result;
+        return res;
     }
 }
