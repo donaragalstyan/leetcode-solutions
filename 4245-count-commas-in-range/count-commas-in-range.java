67fe9d802ac7208bbe6d7693f8d1839f66d3f8f4
Donara Galstyan <donna.galstyan.31@gmail.com>
2026-09-07 19:50:46 -0700

    Time: 6 ms (11.53%) | Memory: 42.6 MB (57.08%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,8 +1,21 @@
 class Solution {
     public int countCommas(int n) {
-        if (n < 1000){
+        int count=0;
+        int num=n;
+        int comma=0;
+        while(num>0){
+            int rem=num%10;
+            num=num/10;
+            count++;
+        }
+        if(count>=4){
+            for(int i=1000;i<=n;i++){
+                comma=i-1000+1;
+            }
+        }
+        if(count<=3){
             return 0;
         }
-        return n - 999;
+        return comma;
     }
 }
